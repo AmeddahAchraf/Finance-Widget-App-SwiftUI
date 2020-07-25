2c52e420066cd13c9bdbfafedff32aa104931c3e
Ameddah Achraf <fm_ameddah@esi.dz>
2020-07-25 18:27:46 +0100

    Initial Commit

---
+++ Finance/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Finance
+//
+//  Created by BqNqNNN on 7/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by BqNqNNN on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
